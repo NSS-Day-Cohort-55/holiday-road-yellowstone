4ed266fa30b159304ae6300d5535fa9b295b19c1
eo-lind <eo.lind@gmail.com>
2022-03-22 09:48:44 -0500

    added dropdown.js and some css to navdropdown.css

---
+++ scripts/dropdown.js
@@ -0,0 +1,35 @@
+// HTML snippets for dropdowns
+
+// Nested dropdown:
+/*
+
+<ul id="menu">
+      <li class="parent">
+        Choose State
+        <ul class="child">
+          <li class="parent">
+            <a href="#">State Name <span class="expand">&#8250;</span></a>
+            <ul class="child">
+              <li><a href="#">Park Name</a></li>
+            </ul>
+          </li>
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+*/
+
+// Single-level dropdown:
+/*
+
+<ul id="menu">
+      <li class="parent">
+        Choose State
+        <ul class="child">
+          <li class="parent">
+            <a href="#">State/Park/Attraction/Restaurant Name <span class="expand">&#8250;</span></a>
+          </li>
+    </ul>
+
+*/
