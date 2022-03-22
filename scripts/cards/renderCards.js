626859beefd3e5471beeae4aca144de4e2f646b6
larsencs <quietus77@gmail.com>
2022-03-22 09:30:07 -0500

    cards are now working with the park api

--- scripts/cards/renderCards.js
+++ scripts/cards/renderCards.js
@@ -4,12 +4,27 @@ import { getBasicWeather } from "../weather/WeatherDataManager.js"
 import { settings } from "../Settings.js"
 
 export const displayCards = () =>{
-
+    let dataToRender = ""
+    //pulls in data from park api and formats/renders it into HTML
     parkData(settings.npsKey, "stateCode=tn" )
         .then(dataFromPark =>{
-            // console.log(typeof dataFromPark)
-            document.querySelector(".test").innerHTML += dataFromPark.data.map(data => data.description)
-            document.querySelector(".test").innerHTML += dataFromPark.data.map(data => data.fullName)
-        })
-        
+            const dataArr = dataFromPark.data
+            for(let i = 0; i < 4; i++){
+                dataToRender += `<div class="panel" id="panel" style="background-image: url('${dataFromPark.data[i].images[0].url}');">`
+                dataToRender += `<h2 class="panel-title">${dataFromPark.data[i].fullName}</h2>`
+                dataToRender += '<div class="text-box">'
+                dataToRender += '<p>Five day forecast:</p>'
+                dataToRender += '<p>Monday:</p>'
+                dataToRender += '<p>Tuesday:</p>'
+                dataToRender += '<p>Wednesday:</p>'
+                dataToRender += '<p>Thursday:</p>'
+                dataToRender += '<p>Friday:</p>'
+                dataToRender += `<h3>${dataFromPark.data[i].fullName}</h3>`
+                dataToRender += `<p>${dataFromPark.data[i].description}</p>`
+                dataToRender += '<button class="save-el">Save</button>'
+                dataToRender += '</div>'
+                dataToRender += '</div>'  
+            }
+            document.querySelector(".card-container-el").innerHTML += dataToRender            
+        })      
 }
